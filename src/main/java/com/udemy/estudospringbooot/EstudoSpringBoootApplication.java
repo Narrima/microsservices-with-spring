3a6607d69719f771358b3b37bfb2846b3ec766f3
suzana.marinho <suzana.marinho@sicoob.com.br>
2023-04-18 18:28:02 -0300

    Primeiro commit

---
+++ src/main/java/com/udemy/estudospringbooot/EstudoSpringBoootApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.estudospringbooot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringBoootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringBoootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/estudospringbooot/EstudoSpringBoootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.estudospringbooot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringBoootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
